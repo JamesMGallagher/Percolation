5280c1253be38ca5fc883ee02bcbee1d78a0aae6
James Gallagher <jamesgallagher@gmx.us>
2015-03-22 17:23:10 +0000

    Import from Bitbucket
    Manual import from bitbucket repository

---
+++ PercolationStats.java
@@ -0,0 +1,119 @@
+
+/**
+ * @author James Gallagher <james.gallagher@gmx.us>
+ * @version 1.0
+ * @since 2014-06-24
+ */
+public class PercolationStats {
+
+    private final int N;
+    private final int T;
+    private final double[] results;
+
+    /**
+     * Performs T independent computational experiments on an N-by-N grid
+     *
+     * @param N the dimension of the grid
+     * @param T the number of experiments to be performed
+     * <dt><b>Precondition:</b><dd> N,T are positive
+     */
+    public PercolationStats(final int N, final int T) {
+        this.N = N;
+        this.T = T;
+        int i;
+        int j;
+        int count;
+        int tests = 0;
+        Percolation percolation;
+        PercolationVisualizer PV = new PercolationVisualizer();
+        results = new double[T];
+
+        while (tests < T) {
+            count = 0;
+            percolation = new Percolation(N);
+            while (!percolation.percolates()) {
+                i = 1 + StdRandom.uniform(N);
+                j = 1 + StdRandom.uniform(N);
+
+                if (!percolation.isOpen(i, j)) {
+                    percolation.open(i, j);
+                    count++;
+                    PV.draw(percolation, N);
+                    PV.show(40);
+
+                }
+
+            }
+            results[tests] = (double) count / (N * N);
+            tests++;
+        }
+
+    }
+
+    /**
+     * Returns the sample mean of percolation threshold
+     *
+     * @return the sample mean of percolation threshold
+     */
+    public double mean() {
+
+        return StdStats.mean(results);
+    }
+
+    /**
+     * Returns sample standard deviation of percolation threshold
+     *
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(results);
+    }
+
+    /**
+     * Returns half the range of the 95% confidence interval
+     *
+     * @return half the range of the 95% confidence interval
+     */
+    private double halfInterval() {
+        return 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    /**
+     * Returns the lower bound of the 95% confidence interval
+     *
+     * @return the lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - halfInterval();
+    }
+
+    /**
+     * Returns the upper bound of the 95% confidence interval
+     *
+     * @return the upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + halfInterval();
+    }
+
+    /**
+     * Test client.
+     *
+     * @param args unused
+     */
+    public static void main(String[] args) {
+
+        PercolationStats percolationStats;
+        percolationStats = new PercolationStats(30, 1);
+
+        StdOut.print(percolationStats.mean());
+        StdOut.print("\n");
+        StdOut.print(percolationStats.stddev());
+        StdOut.print("\n");
+        StdOut.print(percolationStats.confidenceLo());
+        StdOut.print("\n");
+        StdOut.print(percolationStats.confidenceHi());
+
+    }
+
+}
